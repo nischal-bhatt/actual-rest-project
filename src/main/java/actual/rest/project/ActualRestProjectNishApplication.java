9a80fbcdec2c77961287934c33c738519abd3ff8
nischal <nischal.bhatt.87@gmail.com>
2022-03-08 11:42:29 +0800

    new project

---
+++ src/main/java/actual/rest/project/ActualRestProjectNishApplication.java
@@ -0,0 +1,13 @@
+package actual.rest.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActualRestProjectNishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActualRestProjectNishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/actual/rest/project/ActualRestProjectNishApplicationTests.java
@@ -0,0 +1,13 @@
+package actual.rest.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActualRestProjectNishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
